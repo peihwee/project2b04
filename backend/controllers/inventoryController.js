8ed6b9f2779c8e6ad452705c70fa9a33f329550f
peihwee <puaywee@gmail.com>
2022-11-17 11:50:20 +0800

    Add Query Examples #7

--- backend/controllers/inventoryController.js
+++ backend/controllers/inventoryController.js
@@ -1,9 +1,36 @@
+const connection = require("../db");
+
 module.exports.getAllInventory = function(req, res, next)
 {
-    res.send("Get All Inventory");
+    const sql = "SELECT * FROM message";
+    const value = [];
+    
+    connection.promise().query(sql, value)
+    .then(([rows,fields]) => {
+        console.log(rows);
+        res.json(rows);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
 }
 
 module.exports.getInventoryByID = function(req, res, next)
 {
     res.send("getInventoryByID");
 }
+
+module.exports.addInventory = function(req, res, next) {
+
+    const sql = "INSERT INTO message (`message`, `userId`) VALUES (?, ?)";
+    const value = [req.body.message, req.body.userId];
+
+    connection.promise().query(sql, value)
+    .then(([rows,fields]) => {
+        console.log(rows);
+        res.json(rows);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
+};
--- backend/routes/inventoryRoutes.js
+++ backend/routes/inventoryRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/inventoryController");
 router.get("/", controller.getAllInventory);
 router.get("/:id", controller.getInventoryByID);
-
+router.post("/", controller.addInventory);
 module.exports = router;
 
